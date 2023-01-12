642ab5702b7e492f72e2ebcddab0607d0431ef02
Fleming <ryaflem@amazon.com>
2023-01-12 09:47:08 -0800

    added github downloadURL

--- Tot Batch Submission Tool.user.js
+++ Tot Batch Submission Tool.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         Tot Batch Submission Tool
 // @namespace    http://tampermonkey.net/
-// @version      2.1
-// @description  Some controls to assist with coding ToT
+// @version      2.4
+// @description  Some controls to assist with coding ToT - Updated 1-12-2023
 // @author       David Elmkies (delmkies) / edited by jorascuc@amazon.com Filters Edit by Ryaflem@amazon.com
 // @match        fclm-portal.amazon.com/employee/*
 // @require      https://cdn.jsdelivr.net/npm/vue@2.6.14
-
+//@downloadURL   https://github.com/ryanqfleming/EasyTot/blob/main/Tot%20Batch%20Submission%20Tool.user.js
 // @grant        none
 // @run-at       document-end
 // ==/UserScript==
